8f3b420123f6abe4421039eb5a43a394fa36e656
HuangZhenQiu <phuang@zobristinc.com>
2015-01-11 18:27:21 -0800

    Fix HueEntity format issue. Hue message value should not be string "".

--- framework/src/main/java/edu/uci/eecs/wukong/framework/entity/HueEntity.java
+++ framework/src/main/java/edu/uci/eecs/wukong/framework/entity/HueEntity.java
@@ -3,21 +3,24 @@ package edu.uci.eecs.wukong.framework.entity;
 import java.util.Map;
 import java.util.HashMap;
 public class HueEntity implements Entity {
-	private Map<String, String> content;
+	private boolean isOn;
+	private Map<String, Object> content;
 	
 	public HueEntity() {
-		content = new HashMap<String, String>();
-		content.put("on", "false");
+		content = new HashMap<String, Object>();
+		isOn = false;
+		content.put("on", isOn);
 	}
 	
 	public HueEntity(int sat, int bri, int hue) {
-		content.put("on", "true");
-		content.put("sat", Integer.toString(sat));
-		content.put("bri", Integer.toString(bri));
-		content.put("hue", Integer.toString(hue));
+		content = new HashMap<String, Object>();
+		isOn = true;
+		content.put("on", sat);
+		content.put("bri", bri);
+		content.put("hue", hue);
 	}
 	
-	public Map<String, String> getContent() {
+	public Map<String, Object> getContent() {
 		return content;
 	}
 }

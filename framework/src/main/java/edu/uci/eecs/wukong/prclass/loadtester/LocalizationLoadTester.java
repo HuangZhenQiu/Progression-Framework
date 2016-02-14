2d6d401e402b54e2fd78b6608252147da0d00b39
HuangZhenQiu <peter@dhcp-v043-161.mobile.uci.edu>
2016-02-13 21:25:51 -0800

    Fix constructor bug of loadtester

--- framework/src/main/java/edu/uci/eecs/wukong/prclass/loadtester/LocalizationLoadTester.java
+++ framework/src/main/java/edu/uci/eecs/wukong/prclass/loadtester/LocalizationLoadTester.java
@@ -9,6 +9,7 @@ import edu.uci.eecs.wukong.framework.api.Extension;
 import edu.uci.eecs.wukong.framework.model.DataType;
 import edu.uci.eecs.wukong.framework.model.PropertyType;
 import edu.uci.eecs.wukong.framework.prclass.PipelinePrClass;
+import edu.uci.eecs.wukong.framework.prclass.PrClassMetrics;
 import edu.uci.eecs.wukong.framework.property.Response;
 import edu.uci.eecs.wukong.framework.property.Location;
 
@@ -19,8 +20,8 @@ public class LocalizationLoadTester extends PipelinePrClass {
 	@WuProperty(id = 1, name = "location", type = PropertyType.Output)
 	private Location location;
 
-	public LocalizationLoadTester() {
-		super("LoadTester", false);
+	public LocalizationLoadTester(PrClassMetrics metrics) {
+		super("LoadTester", metrics);
 	}
 
 	@Override

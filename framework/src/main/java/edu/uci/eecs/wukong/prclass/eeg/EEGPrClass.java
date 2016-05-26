94dbd7b4b3926cbf6f90d5f118c745f67df1b334
HuangZhenQiu <peter@dhcp-v050-047.mobile.uci.edu>
2016-05-26 13:46:07 -0700

    Add id for EEGPrClass

--- framework/src/main/java/edu/uci/eecs/wukong/prclass/eeg/EEGPrClass.java
+++ framework/src/main/java/edu/uci/eecs/wukong/prclass/eeg/EEGPrClass.java
@@ -3,12 +3,14 @@ package edu.uci.eecs.wukong.prclass.eeg;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.uci.eecs.wukong.framework.annotation.WuClass;
 import edu.uci.eecs.wukong.framework.annotation.WuProperty;
 import edu.uci.eecs.wukong.framework.api.Extension;
 import edu.uci.eecs.wukong.framework.model.DataType;
 import edu.uci.eecs.wukong.framework.model.PropertyType;
 import edu.uci.eecs.wukong.framework.prclass.PipelinePrClass;
 
+@WuClass(id = 10201)
 public class EEGPrClass extends PipelinePrClass {
 	@WuProperty(name = "raw", id = 0, type = PropertyType.Input, dtype = DataType.Buffer)
 	private short raw;

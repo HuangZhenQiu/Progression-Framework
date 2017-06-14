a6cfd2c6c8f0240d94191da2c2d4f052ddb886a9
B.L.Tsai <b95008@csie.ntu.edu.tw>
2017-06-14 16:23:40 -0700

    Fix Topic Model; Add Random Forest customized predict function

---
+++ flink-server/src/main/java/edu/uci/eecs/wukong/framework/RandomForest.java
@@ -0,0 +1,98 @@
+package edu.uci.eecs.wukong.framework;
+
+import com.google.common.collect.Lists;
+import quickml.data.*;
+import quickml.data.instances.ClassifierInstance;
+
+import quickml.supervised.*;
+import quickml.supervised.ensembles.randomForest.randomDecisionForest.*;
+import quickml.supervised.tree.attributeIgnoringStrategies.*;
+import quickml.supervised.tree.decisionTree.*;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.zip.GZIPInputStream;
+
+public class RandomForest {
+    private RandomDecisionForest randomForest;
+
+    public RandomForest(){
+        String filename = "featurestream.txt";
+        randomForest = new RandomDecisionForestBuilder<>(new DecisionTreeBuilder<>()).buildPredictiveModel(getSvmInstances(filename));
+    }
+
+    public int predictByFinalFeatures(double[] finalFeatures){
+        AttributesMap attributes = new AttributesMap();
+        for (int i = 0; i < finalFeatures.length; ++i){
+            attributes.put(Integer.toString(i+1), finalFeatures[i]);
+        }
+        int ret = Integer.parseInt(randomForest.predict(attributes).toString());
+        System.out.println("Prediction: " + ret);
+        return ret;
+    }
+
+//    public static void main(String[] args) throws IOException {
+//        final BufferedReader br = new BufferedReader(new InputStreamReader((new GZIPInputStream(RandomForest.class.getClassLoader().getResourceAsStream("iris.data.gz")))));
+//        final List<ClassifierInstance> irisDataset = Lists.newLinkedList();
+//
+//        String[] headings = new String[]{"sepal-length", "sepal-width", "petal-length", "petal-width"};
+//
+//        String line = br.readLine();
+//        while (line != null) {
+//            String[] splitLine = line.split(",");
+//
+//            AttributesMap attributes = AttributesMap.newHashMap();
+//            for (int x = 0; x < splitLine.length - 1; x++) {
+//                attributes.put(headings[x], Double.valueOf((String)splitLine[x]));
+//            }
+//            irisDataset.add(new ClassifierInstance(attributes, splitLine[splitLine.length - 1]));
+//            line = br.readLine();
+//        }
+//
+//        final RandomDecisionForest randomForest = new RandomDecisionForestBuilder<>(new DecisionTreeBuilder<>()
+//                // The default isn't desirable here because this dataset has so few attributes
+//                .attributeIgnoringStrategy(new IgnoreAttributesWithConstantProbability(0.2)))
+//                .buildPredictiveModel(irisDataset);
+//
+//        AttributesMap attributes = new AttributesMap();
+//        attributes.put("sepal-length", 5.84);
+//        attributes.put("sepal-width", 3.05);
+//        attributes.put("petal-length", 3.76);
+//        attributes.put("petal-width", 1.20);
+//        System.out.println("Prediction: " + randomForest.predict(attributes));
+//        for (ClassifierInstance instance : irisDataset) {
+//            System.out.println("classification: " + randomForest.getClassificationByMaxProb(instance.getAttributes()));
+//        }
+//    }
+
+    public List<ClassifierInstance> getSvmInstances(String filename) {
+        List<ClassifierInstance> instances = Lists.newArrayList();
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(filename);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            for (String line; (line = br.readLine()) != null; ) {
+                List<String> rawInstance = Arrays.asList(line.split(" "));
+                Double label = Double.valueOf(rawInstance.get(0));
+                AttributesMap map = AttributesMap.newHashMap();
+
+                for (String rawAttributeAndValue : rawInstance.subList(1, rawInstance.size())) {
+                    String[] attributeAndValue = rawAttributeAndValue.split(":");
+                    String attribute = attributeAndValue[0];
+                    String value = attributeAndValue[1];
+                    try {
+                        //add numeric variable as Double
+                        map.put(attribute, Double.parseDouble(value));
+                    } catch (NumberFormatException e) {
+                        //add categorical variable as String
+                        map.put(attribute, value);
+                    }
+
+                }
+                instances.add(new ClassifierInstance(map, label));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        return instances;
+    }
+}
--- flink-server/src/main/java/edu/uci/eecs/wukong/framework/TopicModel.java
+++ flink-server/src/main/java/edu/uci/eecs/wukong/framework/TopicModel.java
@@ -16,99 +16,240 @@ public class TopicModel {
     private Pipe pipe;
     private double alpha = 0.1;
     private double beta = 0.01;
+    private int numTopics;
+    private double[] activityArray;
+    private double[][] topicWordDist;
 
     public TopicModel() throws IOException {
-        // Begin by importing documents from text to feature sequences
         ArrayList<Pipe> pipeList = new ArrayList<Pipe>();
 
-        // Pipes
-        pipeList.add( new SvmLight2FeatureVectorAndLabel() );
-        InstanceList instances = new InstanceList (new SerialPipes(pipeList));
+        pipeList.add(new SvmLight2FeatureSequence());
+        InstanceList instances = new InstanceList(new SerialPipes(pipeList));
 
         InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("lda.txt");
         BufferedReader fileReader = new BufferedReader(new InputStreamReader(inputStream));
-        instances.addThruPipe(new CsvIterator (fileReader, Pattern.compile("^(\\S*)$"),1, 0, 0));
+        instances.addThruPipe(new CsvIterator(fileReader, Pattern.compile("^(.*)$"), 1, 0, 0));
 
-        // Create a model with 100 topics, alpha_t = 0.01, beta_w = 0.01
-        //  Note that the first parameter is passed as the sum over topics, while
-        //  the second is the parameter for a single dimension of the Dirichlet prior.
-        int numTopics = ActivityClass.values().length;
-        ParallelTopicModel model = new ParallelTopicModel(numTopics, alpha*numTopics, beta);
+        this.numTopics = ActivityClass.values().length;
+        this.model = new ParallelTopicModel(numTopics, alpha * numTopics, beta);
 
         model.addInstances(instances);
 
-        // Use two parallel samplers, which each look at one half the corpus and combine
-        //  statistics after every iteration.
         model.setNumThreads(1);
 
-        // Run the model for 50 iterations and stop (this is for testing only,
-        //  for real applications, use 1000 to 2000 iterations)
         model.setNumIterations(1500);
         model.setRandomSeed(1);
         model.estimate();
-        pipe = instances.getPipe();
-        inferencer = model.getInferencer();
 
+        activityArray = getActivityArray(instances);
+        topicWordDist = getTopicToWordDistribution();
 
-//        // Show the words and topics in the first instance
-//
-//        // The data alphabet maps word IDs to strings
-//        Alphabet dataAlphabet = instances.getDataAlphabet();
-//
-//        FeatureSequence tokens = (FeatureSequence) model.getData().get(0).instance.getData();
-//        LabelSequence topics = model.getData().get(0).topicSequence;
-//
-//        Formatter out = new Formatter(new StringBuilder(), Locale.US);
-//        for (int position = 0; position < tokens.getLength(); position++) {
-//            out.format("%s-%d ", dataAlphabet.lookupObject(tokens.getIndexAtPosition(position)), topics.getIndexAtPosition(position));
-//        }
-//        System.out.println(out);
-//
-//        // Estimate the topic distribution of the first instance,
-//        //  given the current Gibbs state.
-//        double[] topicDistribution = model.getTopicProbabilities(0);
-//
-//        // Get an array of sorted sets of word ID/count pairs
-//        ArrayList<TreeSet<IDSorter>> topicSortedWords = model.getSortedWords();
-//
-//        // Show top 5 words in topics with proportions for the first document
-//        for (int topic = 0; topic < numTopics; topic++) {
-//            Iterator<IDSorter> iterator = topicSortedWords.get(topic).iterator();
-//
-//            out = new Formatter(new StringBuilder(), Locale.US);
-//            out.format("%d\t%.3f\t", topic, topicDistribution[topic]);
-//            int rank = 0;
-//            while (iterator.hasNext() && rank < 5) {
-//                IDSorter idCountPair = iterator.next();
-//                out.format("%s (%.0f) ", dataAlphabet.lookupObject(idCountPair.getID()), idCountPair.getWeight());
-//                rank++;
-//            }
-//            System.out.println(out);
-//        }
-//
-//        // Create a new instance with high probability of topic 0
-//        StringBuilder topicZeroText = new StringBuilder();
-//        Iterator<IDSorter> iterator = topicSortedWords.get(0).iterator();
-//
-//        int rank = 0;
-//        while (iterator.hasNext() && rank < 5) {
-//            IDSorter idCountPair = iterator.next();
-//            topicZeroText.append(dataAlphabet.lookupObject(idCountPair.getID()) + " ");
-//            rank++;
-//        }
-//
-//        // Create a new instance named "test instance" with empty target and source fields.
-//        InstanceList testing = new InstanceList(instances.getPipe());
-//        testing.addThruPipe(new Instance(topicZeroText.toString(), null, "test instance", null));
-//
-//        TopicInferencer inferencer = model.getInferencer();
-//        double[] testProbabilities = inferencer.getSampledDistribution(testing.get(0), 10, 1, 5);
-//        System.out.println("0\t" + testProbabilities[0]);
+//        pipe = instances.getPipe();
+//        inferencer = model.getInferencer();
     }
 
-    public double[] predict(ActivityDataStream.ActivityWindow activityWindow) {
-        InstanceList newDoc = new InstanceList(pipe);
-        newDoc.addThruPipe(new Instance(activityWindow.toString(), null, "test", null));
-        return inferencer.getSampledDistribution(newDoc.get(0), 10, 1, 5);
+    public double[] inferHighLevelFeatures(int[] basicFeature){
+        double[] highLevelFeature = new double[basicFeature.length];
+
+
+        int index = 0;
+        for (double[] row : topicWordDist){
+            int productIndex = 0;
+            double innerProduct = 0.0;
+            for (double product: row){
+                innerProduct += (basicFeature[productIndex] * Math.log1p(row[productIndex]));
+                productIndex++;
+            }
+            highLevelFeature[index] = innerProduct * activityArray[index];
+            index++;
+        }
+        return highLevelFeature;
+    }
+
+
+
+//    public double[] predict(ActivityDataStream.ActivityWindow activityWindow) {
+//        InstanceList newDoc = new InstanceList(pipe);
+//        newDoc.addThruPipe(new Instance(activityWindow.toString(), null, "test", null));
+//        return inferencer.getSampledDistribution(newDoc.get(0), 10, 1, 5);
+//    }
+
+    private double[] getActivityArray(InstanceList instances){
+        int numSample = instances.size();
+        int [] ytrain = new int[numSample];
+        int [] topic_array = new int[numTopics];
+        for (int i = 0; i < instances.size(); ++i) {
+            Instance instance = instances.get(i);
+            int topic = Integer.parseInt(instance.getTarget().toString());
+            ytrain[i] = topic;
+            topic_array[topic]++;
+        }
+
+        double[][] docTopics = this.getDocTopicDistribution();
+        int[][] topic_act = new int[numTopics][numTopics];
+        for (int i = 0; i < docTopics.length; ++i){
+            double[] elems = docTopics[i];
+            int bestIdx = -1;
+            double max = Double.NEGATIVE_INFINITY;
+            for (int j = 0; j < elems.length; j++) {
+                double elem = elems[j];
+                if (elem > max) {
+                    max = elem;
+                    bestIdx = j;
+                }
+            }
+            topic_act[bestIdx][ytrain[i]]++;
+        }
+
+        int[] map1 = new int[numTopics];
+        for (int i = 0 ; i < topic_act.length; ++i){
+            int[] elems = topic_act[i];
+            int bestIdx = -1;
+            int max = Integer.MIN_VALUE;
+            for (int j = 0; j < elems.length; j++) {
+                int elem = elems[j];
+                if (elem > max) {
+                    max = elem;
+                    bestIdx = j;
+                }
+            }
+            map1[i] = bestIdx;
+        }
+
+        double[] act_array = new double[numTopics];
+        for (int i = 0; i < numTopics; i++){
+            act_array[i] = (double)(topic_array[map1[i]]) / numSample;
+        }
+
+        ytrain = topic_array = map1 = null;
+        docTopics = null;
+        return act_array;
+    }
+
+//    public static void main(String[] args) throws Exception {
+//        TopicModel m = new TopicModel();
+//
+//    }
+
+
+    private double[][] getTopicToWordDistribution(){
+
+        int[][] typeTopicCounts = this.model.getTypeTopicCounts();
+        int topicMask = this.model.topicMask, topicBits = this.model.topicBits;
+        int numTypes = typeTopicCounts.length;
+        double[][] topicWords = new double [numTopics][numTypes];
+
+
+        // Initialize the tree sets
+        for (int topic = 0; topic < numTopics; topic++) {
+            double sum = 0.0;
+            for (int type = 0; type < numTypes; type++) {
+
+                int[] topicCounts = typeTopicCounts[type];
+
+                double weight = beta;
+
+                int index = 0;
+                while (index < topicCounts.length &&
+                        topicCounts[index] > 0) {
+
+                    int currentTopic = topicCounts[index] & topicMask;
+
+
+                    if (currentTopic == topic) {
+                        weight += topicCounts[index] >> topicBits;
+                        break;
+                    }
+
+                    index++;
+                }
+
+                topicWords[topic][type] = weight;
+                sum += weight;
+            }
+            for (int type = 0; type < numTypes; type++){
+                topicWords[topic][type] /= sum;
+            }
+        }
+
+        typeTopicCounts = null;
+        return topicWords;
+    }
+
+    private double[][] getDocTopicDistribution(){
+
+        int numDocs = this.model.getData().size();
+        double[][] docTopics = new double [numDocs][];
+
+
+        // Initialize the tree sets
+        for (int doc = 0; doc < numDocs; doc++) {
+            docTopics[doc] = this.model.getTopicProbabilities(doc);
+        }
+
+        return docTopics;
+    }
+}
+
+class SvmLight2FeatureSequence extends Pipe{
+    private static final long serialVersionUID = 1L;
+
+    public SvmLight2FeatureSequence () {
+        super (new Alphabet(), new LabelAlphabet());
+    }
+
+    @Override public Instance pipe(Instance carrier) {
+        // we expect the data for each instance to be
+        // a line from the SVMLight format text file
+        String dataStr = (String)carrier.getData();
+
+        // ignore comments at the end
+        if (dataStr.contains("#")) {
+            dataStr = dataStr.substring(0, dataStr.indexOf('#'));
+        }
+
+        String[] terms = dataStr.split("\\s+");
+
+        String classStr = terms[0];
+        // In SVMLight +1 and 1 are the same label.
+        // Adding a special case to normalize...
+        if (classStr.equals("+1")) {
+            classStr = "1";
+        }
+        Label label = ((LabelAlphabet)getTargetAlphabet()).lookupLabel(classStr, true);
+        carrier.setTarget(label);
+
+        // the rest are feature-value pairs
+//        ArrayList<Integer> indices = new ArrayList<Integer>();
+//        ArrayList<Integer> values = new ArrayList<Integer>();
+        FeatureSequence featureSequence =
+                new FeatureSequence ((Alphabet) getDataAlphabet(), terms.length);
+        for (int termIndex = 1; termIndex < terms.length; termIndex++) {
+            if (!terms[termIndex].equals("")) {
+                String[] s = terms[termIndex].split(":");
+                if (s.length != 2) {
+                    throw new RuntimeException("invalid format: " + terms[termIndex] + " (should be feature:value)");
+                }
+                String feature = s[0];
+//                int index = getDataAlphabet().lookupIndex(feature, true);
+
+                for (int times = Integer.parseInt(s[1]); times > 0; times--){
+                    featureSequence.add(feature);
+                }
+            }
+        }
+        carrier.setData(featureSequence);
+
+//        assert(indices.size() == values.size());
+//        int[] indicesArr = new int[indices.size()];
+//        double[] valuesArr = new double[values.size()];
+//        for (int i = 0; i < indicesArr.length; i++) {
+//            indicesArr[i] = indices.get(i);
+//            valuesArr[i] = values.get(i);
+//        }
+//
+//        FeatureVector fv = new FeatureVector(getDataAlphabet(), indicesArr, valuesArr);
+//        carrier.setData(fv);
+        return carrier;
     }
 }

fa8baae05eb945fa238b38e3b4a089d07f804650
Peter Huang <hpeter@uber.com>
2017-06-20 16:43:40 -0700

    Add standalone flink job

--- flink-server/src/main/java/edu/uci/eecs/wukong/framework/ec2/ActivityClassificationJob.java
+++ flink-server/src/main/java/edu/uci/eecs/wukong/framework/ec2/ActivityClassificationJob.java
@@ -1,5 +1,11 @@
 package edu.uci.eecs.wukong.framework.ec2;
 
+import org.apache.flink.streaming.api.scala.DataStream;
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment;
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+
+import java.util.Properties;
+
 /**
  * The flink job that consume data from amazon-kinesis cluster. It does the same activity classification as
  * FlinkServer. The difference is that it run as a distributed job, and it uses session window for each host
@@ -7,4 +13,17 @@ package edu.uci.eecs.wukong.framework.ec2;
  *
  */
 public class ActivityClassificationJob {
+
+    public void main(String[] args) {
+        Properties consumerConfig = new Properties();
+        /*consumerConfig.put(ConsumerConfigConstants.AWS_REGION, "us-east-1");
+        consumerConfig.put(ConsumerConfigConstants.AWS_ACCESS_KEY_ID, "aws_access_key_id");
+        consumerConfig.put(ConsumerConfigConstants.AWS_SECRET_ACCESS_KEY, "aws_secret_access_key");
+        consumerConfig.put(ConsumerConfigConstants.STREAM_INITIAL_POSITION, "LATEST");
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<String> kinesis = env.addSource(new FlinkKinesisConsumer<>(
+                "kinesis_stream_name", new SimpleStringSchema(), consumerConfig));*/
+    }
 }
